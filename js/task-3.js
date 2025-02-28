20670160a65d6ac341a14aea2e93e7f2e00e6570
Anna Gandzulevich <gandzulevich@gmail.com>
2025-02-28 19:47:41 +0200

    push hw 6

--- js/task-3.js
+++ js/task-3.js
@@ -1,26 +1,33 @@
-const profile = {
-  username: 'Jacob',
-  playTime: 300,
+class StringBuilder {
+  #value;
 
-  changeUsername(newName) {
-    this.username = newName;
-  },
+  constructor(value) {
+    this.#value = value;
+  }
 
-  updatePlayTime(hours) {
-    this.playTime += hours;
-  },
+  getValue() {
+    return this.#value;
+  }
 
-  getInfo() {
-    return `${this.username} has ${this.playTime} active hours!`;
-  },
-};
+  padEnd(str) {
+    this.#value += str;
+  }
+
+  padStart(str) {
+    this.#value = str + this.#value;
+  }
+
+  padBoth(str) {
+    this.#value = str + this.#value + str;
+  }
+}
 
 // Перевірка функції
-
-console.log(profile.getInfo()); // "Jacob has 300 active hours!"
-
-profile.changeUsername('Marco');
-console.log(profile.getInfo()); // "Marco has 300 active hours!"
-
-profile.updatePlayTime(20);
-console.log(profile.getInfo()); // "Marco has 320 active hours!"
+const builder = new StringBuilder('.');
+console.log(builder.getValue()); // "."
+builder.padStart('^');
+console.log(builder.getValue()); // "^."
+builder.padEnd('^');
+console.log(builder.getValue()); // "^.^"
+builder.padBoth('=');
+console.log(builder.getValue()); // "=^.^="
